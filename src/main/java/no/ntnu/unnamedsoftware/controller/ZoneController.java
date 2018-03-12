7b7e7e49d665b9e62208a3db088a3950499cdd43
OYSTEBAR-RUSSES\oystebar <oebars@gmail.com>
2018-03-12 10:20:57 +0100

    Eyyyyy

--- src/main/java/no/ntnu/unnamedsoftware/controller/ZoneController.java
+++ src/main/java/no/ntnu/unnamedsoftware/controller/ZoneController.java
@@ -1,5 +1,10 @@
 package no.ntnu.unnamedsoftware.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class ZoneController {
 
 }
