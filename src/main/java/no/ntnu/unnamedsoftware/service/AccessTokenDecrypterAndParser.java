1f93ff0e038881110946834b20d413b2013b58e1
Hallvard-PC\HaIIvard <hydrogen324@gmail.com>
2018-03-13 12:37:03 +0100

    added @Service

--- src/main/java/no/ntnu/unnamedsoftware/service/AccessTokenDecrypterAndParser.java
+++ src/main/java/no/ntnu/unnamedsoftware/service/AccessTokenDecrypterAndParser.java
@@ -2,7 +2,9 @@ package no.ntnu.unnamedsoftware.service;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccessTokenDecrypterAndParser {
 	
 	@Autowired
